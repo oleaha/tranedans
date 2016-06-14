e5af7e833e40cfcc0c4240de3fd0551f7259b7d0
Øyvind Robertsen <oyvind@arcticstyle.net>
2016-06-14 15:15:04 +0200

    Test throw_dice values to be within [1,6]

--- test/test_dice.py
+++ test/test_dice.py
@@ -2,4 +2,7 @@ from src.dice import throw_dice
 
 
 def test_throw_dice_returns_list_of_dice_values():
-    assert len(throw_dice(6)) == 6
+    values = throw_dice(5)
+    assert len(values) == 5
+    for value in values:
+        assert 1 <= value <= 6
