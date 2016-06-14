aa531e3d2aa34c4d18336de4ec8b8fd619ccc4e6
Ole Andreas Hansen <oleaha@me.com>
2016-06-14 14:38:15 +0200

    prompting for initial input

--- src/game.py
+++ src/game.py
@@ -1,7 +1,13 @@
 from dice import throw_dice
 
 
-class Game():
+class Game:
+    def __init__(self, nr_of_players, player_names):
+        self.nr_of_players = nr_of_players
+        self.player_names = player_names
+        print(self.player_names)
+        print(self.nr_of_players)
+
     def loop(self):
         for i in range(10):
-            print(throw_dice())
+            print(throw_dice(5))
--- src/main.py
+++ src/main.py
@@ -1,5 +1,18 @@
 from game import Game
 
+
+def prompt_initial_config():
+    print("Welcome to Yetzee")
+    nr_of_players = int(input("Enter the number of players: "))
+    player_names = []
+    for i in range(nr_of_players):
+        player_names.append(input("Enter name of player " + str((i + 1)) + ": "))
+    return nr_of_players, player_names
+
+
 if __name__ == '__main__':
-    game = Game()
+    nr_of_players, player_names = prompt_initial_config()
+
+    game = Game(nr_of_players=nr_of_players, player_names=player_names)
     game.loop()
+
